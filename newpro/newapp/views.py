5dec3a4ac044ec195723e8f9abce37e56c286446
Aromalsukumar <aromalarom2266@gmail.com>
2024-12-19 16:25:46 +0530

    login

--- newpro/newapp/views.py
+++ newpro/newapp/views.py
@@ -8,7 +8,7 @@ def index(request):
         todo123=request.POST.get("todo")
         todo321=request.POST.get("date")
         todo311=request.POST.get("course")
-        obj=Todoitem(title1=todo123,title2=todo321,title3=tudo311)
+        obj=Todoitem(title1=todo123,title2=todo321,title3=todo311)
         obj.save()
         return redirect(index)
     data=Todoitem.objects.all()
